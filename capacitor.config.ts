083b69ffeaf09dff63cd7fd83830d7922a1eb55f
Rhishavhere <rhishh.me@gmail.com>
2025-07-29 05:36:15 +0530

    build fix

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,13 +1,9 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'app.lovable.ea17fc7d44644022a94464eff0a7cde6',
-  appName: 'rhishdesk-nexus',
+  appId: 'com.rhishav.mydesk',
+  appName: 'mydesk',
   webDir: 'dist',
-  server: {
-    url: 'https://ea17fc7d-4464-4022-a944-64eff0a7cde6.lovableproject.com?forceHideBadge=true',
-    cleartext: true
-  },
   plugins: {
     SplashScreen: {
       launchShowDuration: 2000,
